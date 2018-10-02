163894c2653d442f1423b2cb579a94102d2c729f
Juan José <soyjuanarbol@gmail.com>
2018-10-01 23:54:22 -0500

    Added Standard as linter

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-module.exports = function tabto(input) {
-    if (!input instanceof HTMLInputElement) {
-        console.error('tabto() expect a DOM Input element.');
-        return;
+/* global HTMLInputElement */
+
+module.exports = function tabto (input) {
+  if (!(input instanceof HTMLInputElement)) {
+    console.error('tabto() expect a DOM Input element.')
+    return
+  }
+
+  if (!input.dataset.tabTarget) {
+    console.error('tabto() expect the DOM Input element to have a "data-tab-target" attribute.')
+    return
+  }
+
+  const maxLength = input.maxLength
+  const next = document.querySelector(input.dataset.tabTarget)
+  if (!next || !maxLength) {
+    console.error('tabto() a valid target and a maxLength on the provided input.');
+    return
+  }
+
+  let currentValue = input.value
+  input.addEventListener('keyup', function () {
+    // If the value change, check for tab
+    if (input.value !== currentValue) {
+      if (input.value.length >= maxLength) {
+        next.focus()
+      }
     }
-
-    if (!input.dataset.tabTarget) {
-        console.error('tabto() expect the DOM Input element to have a "data-tab-target" attribute.');
-        return;
-    }
-
-    var maxLength = input.maxLength;
-    var next = document.querySelector(input.dataset.tabTarget);
-    var currentValue = input.value;
-
-    if (!next || !maxLength) {
-        console.error('tabto() a valid target and a maxLength on the provided input.');
-        return;
-    }
-
-    input.addEventListener('keyup', function () {
-        // If the value change, check for tab
-        if (input.value !== currentValue) {
-            if (input.value.length >= maxLength) {
-                next.focus();
-            }
-        }
-
-        currentValue = input.value;
-    });
-};
+    currentValue = input.value
+  })
+}
